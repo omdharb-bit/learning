a143cb8ecee64088a215e68a62c9ac15ab8f873c
omdhar-bit <omdharb@gmail.com>
2025-09-18 12:40:03 +0530

    fgf

--- L12.c/hello.c
+++ L12.c/hello.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main(){
   int arr[5] = {1, 2, 3, 4, 5};
-  int max = arr[4];
+  //int max = arr[4];
   /*printf("%d", arr[3]);
   for (int i = 4; i >= 0;i--){
     printf("%d", arr[i]);
 }*/
 
   for (int i = 0; i<5;i++){
-if(arr[i]<max){
-  max = arr[i];
+if(arr[i] %2) 
 }
-}
-printf("minimum value is %d", max);
+printf("  %d",arr[i]);
 }
