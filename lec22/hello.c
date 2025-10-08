fec86a39e79bf1559342604bc59f674f464eee82
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-10-08 13:49:25 +0530

    f

---
+++ lec22/hello.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+//   char str[] = "hello";
+//   int ln = strlen(str);
+//   int count = 0;
+//   for (int i = 0; i < ln;i++){
+//     count = 0;
+//     for (int j = 0; j < i; j++)
+//     {
+//       if (str[i]==str[j]){
+//       count++;
+// break;
+// }
+// }
+// if (count==0){
+//   printf("%c", str[i]);
+// }
+// }
+
+
+//vowel or consonsant length
+
+// char str[] = "aeioubvc";
+// int ln = strlen(str);
+// int vcount = 0;
+ 
+// int ccount = 0;
+
+// for (int i = 0; i < ln;i++){
+//   if (str[i]=='a' || str[i]=='e'|| str[i]=='i'|| str[i]=='o'|| str[i]=='u'){
+//     vcount++;
+// }
+// else{
+//   ccount++;
+// }
+// printf("%d" , );
+// }
+
+
+
+}
