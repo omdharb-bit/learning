9888cecc86dd1a2ea49c6914a32dfcc26c5d0bd4
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-10-08 15:16:36 +0530

    m

--- lec22/hello.c
+++ lec22/hello.c
@@ -37,6 +37,25 @@ int main(){
 // printf("%d" , );
 // }
 
+//frequency of words
+
+// char str[] = "dhruuv";
+// int freq[256] = {0};
+// int ln = strlen(str);
+// for (int i = 0; i < ln;i++){
+//   freq[str[i]]++;
+// }
+// for (int i = 0; i < 256; i++)
+// {
+//   if (freq[i]>0){
+//     printf("%c: %d\n", i, freq[i]);
+// } 
+// }
+
+//first non repeating character
+
+char str[] = "abbcc";
+int ln = strlen(str);
 
 
 }
