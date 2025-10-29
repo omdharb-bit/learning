01e327d1ae23a1c14b69c2408f6da4bfbc793a0a
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-10-29 12:55:47 +0530

    j

--- lec29/10/hello.c
+++ lec29/10/hello.c
@@ -2,10 +2,24 @@
 
 #include<stdio.h>
 int main(){
-  int arr[5] = {4, 5, 3, 4, 5};
-  int xor = 0;
-  for (int i = 0; i < 5;i++){
-    xor = xor ^ arr[i];
-}
-printf("%d", xor);
+
+//finding unique element
+
+//   int arr[5] = {4,5,3,4,5};
+//   int xor = 0;
+//   for (int i = 0; i < 5;i++){
+//     xor = xor ^ arr[i];
+// }
+// printf(" The unique elements belongs here is %d", xor);
+
+
+//swaping two no. using xor
+int a = 5;
+int b = 3;
+
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+printf("a=%d , b=%d", a, b);
 }
