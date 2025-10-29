5cc7786aeecde1149fb020d0590d6bb3ef5f852d
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-10-29 12:32:04 +0530

    v

--- lec23/hello.c
+++ lec23/hello.c
@@ -1,16 +1,67 @@
-//creating array dynamically
+// creating array dynamically
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+// int main()
+// {
+//   int n;
+//   printf("Enter size: ");
+//   scanf("%d", &n);
+
+//   int *arr = (int *)malloc(n* sizeof(int));
+
+//   printf("Enter %d elements:\n", n);
+//   for (int i = 0; i < n; i++)
+//     scanf("%d", &arr[i]);
+
+   
+// }
+
+// realloc
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//   int n;
+//   printf("Enter size:");
+//   scanf("%d", &n);
+
+//   printf("\nEnter new size:");
+//   int newSize;
+//   scanf("%d", &newSize);
+
+//   int *arr = (int *)realloc(arr, newSize * sizeof(int));
+//   printf("Enter  elements:\n");
+//   for (int i = 0; i < n; i++)
+//   {
+//     scanf("%d", &arr[i]);
+//   }
+//   for (int i = 0; i < n; i++)
+//   {
+//     printf("%d ", arr[i]);
+//   }
+// }
+
+
+//calloc
+
+ 
+int main()
+{
   int n;
   printf("Enter size: ");
   scanf("%d", &n);
 
-  int *arr = (int *)malloc(n * sizeof(int));
+  int *arr = (int *)calloc(n, sizeof(int));
 
   printf("Enter %d elements:\n", n);
-  for(int i = 0; i < n;i++)
+  for (int i = 0; i < n; i++)
     scanf("%d", &arr[i]);
+
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d", arr[i]);
+  }
 }
